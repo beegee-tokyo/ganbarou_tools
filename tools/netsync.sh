69888c039f8d0a65e3b203120b2413b35adb00fe
Bernd Giesecke <bernd@giesecke.tk>
2013-01-11 22:08:31 +0900

    Update tools/netsync.sh
    Updated for Jellybean 4.2

--- tools/netsync.sh
+++ tools/netsync.sh
@@ -1,8 +1,8 @@
 echo ============================================
 echo 'Init sync CM10 with Github'
-echo 'repo init -u git://github.com/beegee-tokyo/ganbarou-jb.git -b jb-4.1'
+echo 'repo init -u git://github.com/beegee-tokyo/ganbarou-jb.git -b jb-4.2'
 echo ============================================
-repo init -u git://github.com/beegee-tokyo/ganbarou-jb.git -b jb-4.1
+repo init -u git://github.com/beegee-tokyo/ganbarou-jb.git -b jb-4.2
 if [ $? -eq 0 ]; then
    echo ============================================
    echo 'Repo init successfull'
@@ -14,7 +14,7 @@ else
    exit $?
 fi
 echo ============================================
-echo 'Sync CM10'
+echo 'Sync CM10.1'
 echo 'repo sync -f'
 echo ============================================
 repo sync -f
@@ -33,15 +33,15 @@ echo ============================================
 echo 'Get prebuilts'
 echo './vendor/cm/get-prebuilts'
 echo ============================================
-./vendor/cm/get-prebuilts
-if [ $? -eq 0 ]; then
-   echo ============================================
-   echo 'Get prebuilts init successfull'
-   echo ============================================
-else
-   echo ============================================
-   echo 'Get prebuilts failed'
-   echo ============================================
-   exit $?
-fi
+#./vendor/cm/get-prebuilts
+#if [ $? -eq 0 ]; then
+#   echo ============================================
+#   echo 'Get prebuilts init successfull'
+#   echo ============================================
+#else
+#   echo ============================================
+#   echo 'Get prebuilts failed'
+#   echo ============================================
+#   exit $?
+#fi
 
