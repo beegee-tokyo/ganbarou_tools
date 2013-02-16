0d78d46ab2783bb9eb6dbe4463cdac4e20bfd316
Bernd Giesecke <bernd@giesecke.tk>
2013-02-16 23:35:47 +0900

    Added script to create links for build scripts and optimized sync script
    Change-Id: Iaa9fee2e445f0d70c2156e8904af95500210b259

---
+++ create_links.sh
@@ -0,0 +1,8 @@
+ln ./tools/changelog.txt ../changelog.txt
+ln ./tools/locsync.sh ../locsync.sh
+ln ./tools/makeit.sh ../makeit.sh
+ln ./tools/merge.sh ../merge.sh
+ln ./tools/patchit.sh ../patchit.sh
+ln ./tools/push.sh ../push.sh
+ln ./tools/sync.sh ../sync.sh
+
