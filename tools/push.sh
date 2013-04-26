fb123f52d21e46d4a4dfb0a8c30bb1e1c73aca04
BeeGee <bernd@giesecke.tk>
2013-04-26 14:10:30 +0800

    Updated push.sh
    Change-Id: I92dbb2373c3e86f2478484526a61cb7c04ff4565

--- tools/push.sh
+++ tools/push.sh
@@ -35,6 +35,45 @@ git commit -a
 git push git@github.com:beegee-tokyo/android_vendor_samsung_p4wifi.git cm-10.1
 cd ../../..
 echo ============================================
+echo 'Push latest changes to beegee-tokyo device_p4'
+echo ============================================
+cd device
+cd samsung
+cd p4
+repo start jellybean .
+git checkout jellybean
+git pull
+git add -A
+git commit -a
+git push git@github.com:beegee-tokyo/android_device_samsung_p4.git jellybean
+cd ../../..
+echo ============================================
+echo 'Push latest changes to beegee-tokyo device_p4wifi'
+echo ============================================
+cd device
+cd samsung
+cd p4wifi
+repo start jellybean .
+git checkout jellybean
+git pull
+git add -A
+git commit -a
+git push git@github.com:beegee-tokyo/android_device_samsung_p4wifi.git jellybean
+cd ../../..
+echo ============================================
+echo 'Push latest changes to beegee-tokyo device_p4-common'
+echo ============================================
+cd device
+cd samsung
+cd p4-common
+repo start jellybean .
+git checkout jellybean
+git pull
+git add -A
+git commit -a
+git push git@github.com:beegee-tokyo/android_device_samsung_p4-common.git jellybean
+cd ../../..
+echo ============================================
 echo 'Push latest changes to beegee-tokyo vendor_n7000'
 echo ============================================
 cd vendor
