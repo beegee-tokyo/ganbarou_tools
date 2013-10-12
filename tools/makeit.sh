81834813bd0ad85802632c9636d02a6527242c9e
beegee=tokyo <bernd@giesecke.tk>
2013-10-12 20:28:16 +0900

    Updated version + cleanup tools and roomservice.xml
    Change-Id: I20a8252d29a800edbbb1a5f0daaebfa3a352c2a3

--- tools/makeit.sh
+++ tools/makeit.sh
@@ -248,28 +248,11 @@ fi
 # all 6 params must be given!
 
 if [ $DO_P750T -eq 0 ]; then
-#   echo -e $CL_MAG"=============================================="$CL_RST
-#   echo -e $CL_MAG"Start the build for GT-P7500 pershoot kernel"$CL_RST
-#   echo -e $CL_MAG"=============================================="$CL_RST
-   res75001=$(date +%s.%N)
-#   export kernelversion="pershoot"
-#   export kernelbuild="cyanogenmod_samsung_p4-jb_defconfig"
-#   . build/envsetup.sh && brunch p4
-#   if [ $? -eq 0 ]; then
-#      echo -e $CL_MAG"=============================================="$CL_RST
-#      echo -e $CL_GRN"Build for GT-P7500 pershoot kernel successfull"$CL_RST
-#      echo -e $CL_MAG"=============================================="$CL_RST
-#      export P750TRESULT=0
-#      ./patchit.sh GT-P7500 GT-P7501 0 p4 pershoot 1
-#   else
-#      echo -e $CL_MAG"=============================================="$CL_RST
-#      echo -e $CL_RED"Build for GT-P7500 pershoot kernel failed"$CL_RST
-#      echo -e $CL_MAG"=============================================="$CL_RST
-#      export P750TRESULT=1
-#   fi
+  res75001=$(date +%s.%N)
    echo -e $CL_MAG"=============================================="$CL_RST
    echo -e $CL_MAG"Start the build for GT-P7500 infamous kernel"$CL_RST
    echo -e $CL_MAG"=============================================="$CL_RST
+   export TARGET_KERNEL_CUSTOM_TOOLCHAIN=arm-eabi-4.6
    export kernelversion="infamous"
    export kernelbuild="infamous_p4_defconfig"
    . build/envsetup.sh && brunch p4
@@ -285,33 +268,17 @@ if [ $DO_P750T -eq 0 ]; then
       echo -e $CL_MAG"=============================================="$CL_RST
       export P750TRESULT=1
    fi
+   export TARGET_KERNEL_CUSTOM_TOOLCHAIN=
    res75002=$(date +%s.%N)
 else
    export P750TRESULT=1
 fi
 if [ $DO_P7500 -eq 0 ]; then
-#   echo -e $CL_MAG"=============================================="$CL_RST
-#   echo -e $CL_MAG"Start the build for GT-P7500 pershoot kernel"$CL_RST
-#   echo -e $CL_MAG"=============================================="$CL_RST
    res75001=$(date +%s.%N)
-#   export kernelversion="pershoot"
-#   export kernelbuild="cyanogenmod_samsung_p4-jb_defconfig"
-#   . build/envsetup.sh && brunch p4
-#   if [ $? -eq 0 ]; then
-#      echo -e $CL_MAG"=============================================="$CL_RST
-#      echo -e $CL_GRN"Build for GT-P7500 pershoot kernel successfull"$CL_RST
-#      echo -e $CL_MAG"=============================================="$CL_RST
-#      export P7500RESULT=0
-#      ./patchit.sh GT-P7500 GT-P7501 1 p4 pershoot 0
-#   else
-#      echo -e $CL_MAG"=============================================="$CL_RST
-#      echo -e $CL_RED"Build for GT-P7500 pershoot kernel failed"$CL_RST
-#      echo -e $CL_MAG"=============================================="$CL_RST
-#      export P7500RESULT=1
-#   fi
    echo -e $CL_MAG"=============================================="$CL_RST
    echo -e $CL_MAG"Start the build for GT-P7500 infamous kernel"$CL_RST
    echo -e $CL_MAG"=============================================="$CL_RST
+   export TARGET_KERNEL_CUSTOM_TOOLCHAIN=arm-eabi-4.6
    export kernelversion="infamous"
    export kernelbuild="infamous_p4_defconfig"
    . build/envsetup.sh && brunch p4
@@ -327,33 +294,17 @@ if [ $DO_P7500 -eq 0 ]; then
       echo -e $CL_MAG"=============================================="$CL_RST
       export P7500RESULT=1
    fi
+   export TARGET_KERNEL_CUSTOM_TOOLCHAIN=
    res75002=$(date +%s.%N)
 else
    export P7500RESULT=1
 fi
 if [ $DO_P7510 -eq 0 ]; then
-#   echo -e $CL_MAG"=============================================="$CL_RST
-#   echo -e $CL_MAG"Start the build for GT-P7510 pershoot kernel"$CL_RST
-#   echo -e $CL_MAG"=============================================="$CL_RST
    res75101=$(date +%s.%N)
-#   export kernelversion="pershoot"
-#   export kernelbuild="cyanogenmod_samsung_p4wifi-jb_defconfig"
-#   . build/envsetup.sh && brunch p4wifi
-#   if [ $? -eq 0 ]; then
-#      echo -e $CL_MAG"=============================================="$CL_RST
-#      echo -e $CL_GRN"Build for GT-P7510 pershoot kernel successfull"$CL_RST
-#      echo -e $CL_MAG"=============================================="$CL_RST
-#      export P7510RESULT=0
-#      ./patchit.sh GT-P7510 GT-P7511 1 p4wifi pershoot 0
-#   else
-#      echo -e $CL_MAG"=============================================="$CL_RST
-#      echo -e $CL_RED"Build for GT-P7510 pershoot kernel failed"$CL_RST
-#      echo -e $CL_MAG"=============================================="$CL_RST
-#      export P7510RESULT=1
-#   fi
    echo -e $CL_MAG"=============================================="$CL_RST
    echo -e $CL_MAG"Start the build for GT-P7510 infamous kernel"$CL_RST
    echo -e $CL_MAG"=============================================="$CL_RST
+   export TARGET_KERNEL_CUSTOM_TOOLCHAIN=arm-eabi-4.6
    export kernelversion="infamous"
    export kernelbuild="infamous_p4wifi_defconfig"
    . build/envsetup.sh && brunch p4wifi
@@ -369,6 +320,7 @@ if [ $DO_P7510 -eq 0 ]; then
       echo -e $CL_MAG"=============================================="$CL_RST
       export P7510RESULT=1
    fi
+   export TARGET_KERNEL_CUSTOM_TOOLCHAIN=
    res75102=$(date +%s.%N)
 else
    export P7510RESULT=1
