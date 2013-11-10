12344bedd85cb141ed1a7fb068788b33f1edec31
beegee=tokyo <bernd@giesecke.tk>
2013-11-11 01:32:37 +0900

    Cleanup and optimize some tools
    Change-Id: Idd52c8a8254a4f9e813aeb44148b13c49e54c376

--- tools/pushtools.sh
+++ tools/pushtools.sh
@@ -6,6 +6,18 @@ cd ../
 find ./$_CURRENTPATH -name *.*~ -exec rm -rf {} \;
 cd $_CURRENTPATH
 echo ============================================
+echo 'Copy batch files back to Ganbarou Tools'
+echo ============================================
+cp changelog.txt ./ganbarou_tools/tools/changelog.txt
+cp makeit.sh ./ganbarou_tools/tools/makeit.sh 
+cp merge.sh ./ganbarou_tools/tools/merge.sh 
+cp patchit.sh ./ganbarou_tools/tools/patchit.sh 
+cp publish.sh ./ganbarou_tools/tools/publish.sh 
+cp push.sh ./ganbarou_tools/tools/push.sh 
+cp pushtools.sh ./ganbarou_tools/tools/pushtools.sh 
+cp sync.sh ./ganbarou_tools/tools/sync.sh 
+cp ./.repo/local_manifests/roomservice.xml ./ganbarou_tools/tools/roomservice.xml 
+echo ============================================
 echo 'Push latest changes to Ganbarou Tools'
 echo ============================================
 cd ganbarou_tools
