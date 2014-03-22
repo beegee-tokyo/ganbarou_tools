331ca2e97f13178247323160150fbdae071ab56a
beegee=tokyo <bernd@giesecke.tk>
2014-03-23 00:48:17 +0900

    Updated changelog, roomservice.xml
    	modified:   create_links.sh
    	modified:   tools/changelog.txt
    	modified:   tools/roomservice.xml
    	new file:   tools/safe_tools.sh

--- create_links.sh
+++ create_links.sh
@@ -5,4 +5,4 @@ cp -f ./tools/merge.sh ../merge.sh
 cp -f ./tools/push.sh ../push.sh
 cp -f ./tools/sync.sh ../sync.sh
 cp -f ./tools/roomservice.xml ../.repo/local_manifests/roomservice.xml
-
+cp -f ./tools/safe_tools.sh ../safe_tools.sh
